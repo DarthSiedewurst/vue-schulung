4327972e9a672e27a716006d39dd5d75cab8f318
Wookie-Monster <Wookie-Monster@blablubblub.de>
2020-01-14 13:55:19 +0100

    commit

--- src/views/Philipp.vue
+++ src/views/Philipp.vue
@@ -1,12 +1,7 @@
 <template>
   <b-container>
-<<<<<<< HEAD
     <img id="wookie_pic" alt="Wookie Monster" src="../assets/wookie_m.jpg" />
     <HelloWorld msg="COOKIES!!! OM NOM NOM NOM NOM!" />
-=======
-    <img alt="Vue logo" src="@/assets/wookie_m.jpg" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
->>>>>>> 4ba86fe8d4345cbc384ed6bca85170383dad5bd3
     <Navigation urlBack="/G" urlForward="/marco" back="G" forward="marco"></Navigation>
   </b-container>
 </template>
