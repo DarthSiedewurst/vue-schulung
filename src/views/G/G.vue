bb875370cb21488dc5eb40c4764b22ffe9958893
Trash <DEB839T@emea.zurich.corp>
2020-01-14 13:39:12 +0100

    added Villains

--- src/views/G/G.vue
+++ src/views/G/G.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+      <b-container>
+      <b-row>
+        <b-col class="col-2">
+          <p><router-link to="/G/heroes">Heroes</router-link></p>
+          <p><router-link to="/G/villains">Villains</router-link></p>
+        </b-col>
+        <b-col class="col-10">
+          <router-view/>
+        </b-col>
+      </b-row>
+      </b-container>
     <Navigation urlBack="/Felix" urlForward="/Philipp" back="Felix" forward="Philipp"></Navigation>
     <center>yey</center> 
   </div>
